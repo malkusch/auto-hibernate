db75ab0a2b36ab9cb7a273b66cd05e4d8df7bc1d
Markus Malkusch <markus@malkusch.de>
2012-08-28 18:39:12 +0200

    no hibernation on network activity

--- src/auto-hibernate.sh
+++ src/auto-hibernate.sh
@@ -3,6 +3,8 @@
 LOCKFILE=/var/lock/auto-hibernate.lock
 TIMESTAMP=/tmp/auto-hibernate.time
 TIMEOUT=30MINUTES
+HIBERNATE=/usr/sbin/hibernate-ram
+LOGGER='logger -t "auto-hibernate"'
 
 function cleanup {
 	rm -f $TIMESTAMP
@@ -14,42 +16,42 @@ function cleanup {
 mkdir $LOCKFILE || exit 1
 set +e
 
-# keine screen session
+# no screen sessions
 sessions=$(ps a | grep screen | grep -v "grep" | wc -l)
 if [ ! $sessions = 0 ]; then
-	logger -t "auto-hibernate" "Screen sessions sind vorhanden"
+	$LOGGER "screen prevents hibernation"
 	cleanup
         exit 1
 fi
 
-# keine SMB-Nutzung
-smbstatus -S | grep machine -A 2 | tail -n1 | egrep -q ".{1,}"
-if [ $? = 0 ]; then
-        logger -t "auto-hibernate" "Samba ist in Nutzung"
+#no network connections
+netstat=$(LC_ALL=C netstat --inet -n -v -a 2>>/dev/null | grep -v LISTEN | wc -l)
+if [ ! $netstat = 2 ]; then
+        $LOGGER "network connections prevents hibernation"
         cleanup
         exit 1
 fi
 
-# kein angemeldeter Benutzer
+# no users
 who -q | tail -n 1 | grep -qv "=0$"
 if [ $? = 0 ]; then
-	logger -t "auto-hibernate" "Benutzer sind angemeldet"
+	$LOGGER "logged in user prevents hibernation"
 	cleanup
 	exit 1
 fi
 
-# obige Bedingungen sind mind. 10 Minuten alt
+# preconditions must be valid for a period of time
 if [ ! -e $TIMESTAMP ]; then
 	touch -d "$TIMEOUT" $TIMESTAMP
 fi
 if [ $TIMESTAMP -nt $LOCKFILE ]; then
-	logger -t "auto-hibernate" "Ruhedauer noch nicht erreicht"
+	$LOGGER "preconditions not old enough"
 	rmdir $LOCKFILE
         set -e
 	exit 1
 fi
 
-# Zeit zum Schlafen gehen
-logger -t "auto-hibernate" "automatisches Schlafen"
-/usr/sbin/hibernate-ram
+# Everything should be fine for hibernation
+$LOGGER "hibernate"
+$HIBERNATE
 cleanup
